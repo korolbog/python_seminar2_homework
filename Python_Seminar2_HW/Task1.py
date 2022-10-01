fcfcbb94a65d128a9eabe884f458d493e31124d8
BogdanKorolev <bkorolevjob@gmail.com>
2022-10-01 11:25:04 +0300

    Task1: minor cosmetic changes to the output.

--- Python_Seminar2_HW/Task1.py
+++ Python_Seminar2_HW/Task1.py
@@ -13,17 +13,14 @@ head_count = 0
 while temp1 % 10 > 0:
     temp1 //= 10
     head_count += 1
-print(f'head_count = {head_count}')
 
 head = int(n)
 head_sum = 0
 for i in range(head_count):
     head_sum += head % 10
     head //= 10
-print(f'head_sum = {head_sum}')
 
 temp2 = str(n)
-print(f'temp2 = {temp2}')
 temp2 = temp2[head_count+1:len(temp2)]
 
 tail = int(temp2)
@@ -32,12 +29,10 @@ tail_count = 0
 while temp3 % 10 > 0:
     temp3 //= 10
     tail_count += 1
-print(f'tail_count = {tail_count}')
 
 tail_sum = 0
 for i in range(tail_count):
     tail_sum += tail % 10
     tail //= 10
-print(f'tail_sum = {tail_sum}')
 
-print(head_sum+tail_sum)
+print(f'Сумма цифр = {head_sum+tail_sum}')
